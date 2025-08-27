b78d0a97f9f5a1416e8d9c83ad7f5e1100929125
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-27 17:09:01 +0400

    Add User <-> Task relationship.

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
+/** @mixin Builder */
 class Task extends Model
 {
     /** @use HasFactory<\Database\Factories\TaskFactory> */
@@ -13,4 +16,9 @@ class Task extends Model
     protected $fillable = [
         'content'
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
