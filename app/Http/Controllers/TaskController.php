1f0a3ff637229ecdd938b58d9f18e98bb52fb5da
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-27 19:48:54 +0400

    Add strict_types mode to all our classes (better late than never).

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -1,33 +1,37 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Http\Controllers;
 
 use App\Http\Requests\TaskCreateRequest;
 use App\Models\Task;
 use App\Services\TaskService;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
 
 class TaskController extends Controller
 {
     public function __construct(protected TaskService $taskService) {}
 
-    public function index()
+    public function index(): View
     {
         $tasks = $this->taskService->tasksForAuthenticatedUser();
         return view('tasks.index', compact('tasks'));
     }
 
-    public function create()
+    public function create(): View
     {
         return view('tasks.create');
     }
 
-    public function store(TaskCreateRequest $request)
+    public function store(TaskCreateRequest $request): RedirectResponse
     {
         $this->taskService->create($request->validated()['content']);
         return to_route('index');
     }
 
-    public function destroy(Task $task)
+    public function destroy(Task $task): RedirectResponse
     {
         $this->taskService->remove($task);
         return to_route('index');
