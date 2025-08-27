3d7cb9461984f05d91298888f6b2d35a563a6da0
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-27 16:48:33 +0400

    Rename 'dashboard' route to 'index' route in all files.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
+    Route::get('/', [TaskController::class, 'index'])->name('index');
     Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
     Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
     Route::get('/tasks/{task}', [TaskController::class, 'show'])->name('tasks.show');
