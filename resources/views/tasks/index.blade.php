c3dbaf7ca01ffdff962301496bf7e0e53cc8be57
UnrealTemplier <jaimore.evseev@gmail.com>
2025-08-27 19:24:15 +0400

    Add 'Add task' and 'Delete' buttons; Adjust overall design.

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,11 +2,17 @@
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900">
 
-                    <x-task-list :tasks="$tasks"/>
-
+                <div class="mt-8 ml-8">
+                    <a href="{{ route('tasks.create') }}">
+                        <x-primary-button>+Add task</x-primary-button>
+                    </a>
                 </div>
+
+                <div class="p-6 text-gray-900">
+                    <x-task-list :tasks="$tasks"/>
+                </div>
+
             </div>
         </div>
     </div>
